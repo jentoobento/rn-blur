9bb369392071013bb401ba862b3f94fd9e2b82ab
jennifer <teriyakibuns@yahoo.com>
2019-07-17 14:37:12 -0700

    working

--- App.js
+++ App.js
@@ -4,11 +4,24 @@ import { BlurView } from "@react-native-community/blur";
 import styles from './styles';
 
 const App = () => {
+  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Blur</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+      <View style={{ height: 40, width: 100, backgroundColor: 'black'}}>
+        <Text style={{ color: 'white'}}>Blurred Text</Text>
+      </View>
       <Image resizeMode="contain" source={require('./images/park.jpg')}/>
-      <BlurView blurType="light" blurAmount={2} />
+      <BlurView blurAmount={3} style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+      }}/>
+      <View style={{ height: 40, width: 100, backgroundColor: 'white'}}>
+        <Text>Unblurred Text</Text>
+      </View>
     </View>
   );
 };
